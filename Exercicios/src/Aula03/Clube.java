8ad40c05e463c2d7cced421125e156d14ab228e6
Andreas Berwaldt <68302791+andreaspsb@users.noreply.github.com>
2025-03-05 22:00:29 +0000

    exercicios da aula 03

---
+++ Exercicios/src/Aula03/Clube.java
@@ -0,0 +1,20 @@
+public class Clube {
+
+    private SocioAtleta[] sociosAtleta;
+    private int qtdSocios;
+
+    public Clube (int tamanho) {
+        this.sociosAtleta = new SocioAtleta[tamanho];
+        this.qtdSocios = 0;
+    }
+
+    public inscreveSocio() {
+        if (qtdSocios == this.qtdSocios.length) {
+            
+        }
+
+        this.sociosAtleta[this.qtdSocios] = ;
+        this.qtdSocios++;
+    }
+
+}
---
+++ Exercicios/src/Aula03/SocioAtleta.java
@@ -0,0 +1,58 @@
+public class SocioAtleta {
+
+    private String nome;
+
+    private int idade;
+
+    private double altura;
+
+    private int categoria;
+
+    public SocioAtleta(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.categoria = this.calcularCategoria();
+    }
+
+    public int calcularCategoria() {
+        int categoria;
+        if (this.idade <= 12) {
+            if (this.altura <= 1.55) {
+                categoria = 10;
+            } else {
+                categoria = 20;
+            }
+        } else {
+            if (this.altura <= 1.68) {
+                categoria = 30;
+            } else {
+                categoria = 40;
+            }
+        }
+
+        return categoria;
+    }
+
+    public revisarAltura(int altura) {
+        this.altura = altura;
+        this.categoria = this.calcularCategoria();
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public double getAltura() {
+        return this.altura;
+    }
+
+    public int getCategoria() {
+        return this.categoria;
+    }
+
+}
